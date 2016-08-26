10ba4ae575b77628b9f155e996bdada40e9a1b41
Philip Youssef <philip.youssef@gmail.com>
2016-08-26 16:30:37 -0700

    Finish Ch 3

--- src/brave_clojure/ch_3.clj
+++ src/brave_clojure/ch_3.clj
@@ -1,14 +1,41 @@
 (ns brave-clojure.ch-3)
 
-;Write a function that takes a number and adds 100 to it.
+;2) Write a function that takes a number and adds 100 to it.
 (defn add-hundred 
   "Adds 100 to a number"
   [num] 
   (+ num 100))
 
-;Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+;3) Write a function, dec-maker, that works exactly like the function inc-maker 
+; except with subtraction:
 ;(def dec9 (dec-maker 9))
 ;(dec9 10)
 ; => 1
 (defn dec-maker [num]
   (fn [x] (- x num)))
+
+;4) Write a function, mapset, that works like map except the return value is a set:
+;(mapset inc [1 1 2 2])
+;; => #{2 3}
+(defn mapset [f l]
+  (into #{} (map f l)))
+
+;5+6) Create a function that’s similar to symmetrize-body-parts except that it has to 
+;work with weird space aliens with radial symmetry. Instead of two eyes, arms, 
+;legs, and so on, they have five.
+(defn matching-multi-part [part n]
+  (defn gen-names [part]
+    (if (re-matches #"^left-.*" (:name part))
+      (map (fn [x] {:name (clojure.string/replace (:name part) #"^left-" (str x "-")) :size (:size part)} ) 
+           (range 1 (+ 1 n) 1))
+      [part]
+      ))
+  (gen-names part))
+
+(defn symmetrize-space-alien
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+              (into final-body-parts (matching-multi-part part 5)))
+            []
+            asym-body-parts))
--- test/brave_clojure/ch_3_test.clj
+++ test/brave_clojure/ch_3_test.clj
@@ -9,3 +9,25 @@
 (deftest dec-maker-test
   (testing "Can we create a function that decrements by 5"
     (is (= ((dec-maker 5) 10) 5))))
+
+(deftest mapset-test
+  (testing "can we map a vector into a set"
+    (is (= #{2 3 4} (mapset inc [1 2 3])))))
+
+(defn in?
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
+(deftest symmetrize-space-alien-test
+  (testing "can we map an aliens body parts"
+    (def parts [{:name "head" :size 3}
+                {:name "left-eye" :size 1}])
+    ; Check that there are 6 parts after symmetrization
+    (def sym-parts (symmetrize-space-alien parts))
+    (is (= 6 (count sym-parts)))
+    ; there should be 5 eyes & 1 head
+    (def part-names (mapv (fn [x] (:name x)) sym-parts))
+    (println (str part-names) )
+    (is (= true (in? part-names "1-eye")))
+    ))
